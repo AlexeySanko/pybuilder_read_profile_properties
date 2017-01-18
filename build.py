56315e919bc4cc288abcb0608213b16affa92cba
AlexeySanko <asanko@braintch.com>
2017-01-18 17:22:13 +0300

    fixes #4

--- build.py
+++ build.py
@@ -25,7 +25,7 @@ use_plugin('pypi:pybuilder_pytest')
 
 
 name = "pybuilder-read-profile-properties"
-version = '0.2.0'
+version = '0.2.1'
 authors = [Author('Alexey Sanko', 'alexeycount@gmail.com')]
 url = 'https://github.com/AlexeySanko/pybuilder_read_profile_properties'
 description = 'Please visit {0} for more information!'.format(url)
